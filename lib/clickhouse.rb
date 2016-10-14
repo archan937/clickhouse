a56567a390b87d3243ba4f550e7bf7652e5b760f
Paul Engel <pm_engel@icloud.com>
2016-10-15 00:10:00 +0200

    Being able to set and get the Clickhouse logger

--- lib/clickhouse.rb
+++ lib/clickhouse.rb
@@ -1,4 +1,13 @@
 require "clickhouse/version"
 
 module Clickhouse
+
+  def self.logger=(logger)
+    @logger = logger
+  end
+
+  def self.logger
+    @logger
+  end
+
 end
--- test/unit/test_clickhouse.rb
+++ test/unit/test_clickhouse.rb
@@ -9,6 +9,21 @@ module Unit
         assert_equal version, Clickhouse::VERSION
         assert File.read(path "CHANGELOG.md").include?("Version #{version} ")
       end
+
+      describe ".logger" do
+        it "returns its instance variable :@logger" do
+          Clickhouse.instance_variable_set :@logger, (logger = mock)
+          assert_equal logger, Clickhouse.logger
+        end
+      end
+
+      describe ".logger=" do
+        it "stores the passed value as the instance variable :@logger" do
+          Clickhouse.logger = (logger = mock)
+          assert_equal logger, Clickhouse.instance_variable_get(:@logger)
+        end
+      end
+
     end
 
   end
